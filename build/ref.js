cc06772eb97ed785cd0d882e87586afc0c16dc9b
ryan <1184518756@qq.com>
2020-10-30 18:23:27 +0800

    update

--- build/ref.js
+++ build/ref.js
@@ -16,12 +16,40 @@ return /******/ (() => { // webpackBootstrap
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "ref": () => /* binding */ ref
+/* harmony export */   "ref": () => /* binding */ ref,
+/* harmony export */   "refs": () => /* binding */ refs
 /* harmony export */ });
 /* harmony import */ var _Ref__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 var ref = function ref(target, vars, mapping) {
   return new _Ref__WEBPACK_IMPORTED_MODULE_0__.default().bind(target, mapping).vars(vars).proxy;
+};
+var refs = function refs() {
+  var targets = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  var vars = arguments.length > 1 ? arguments[1] : undefined;
+  var mapping = arguments.length > 2 ? arguments[2] : undefined;
+  var ref = new _Ref__WEBPACK_IMPORTED_MODULE_0__.default();
+  ref.vars(vars);
+
+  _toConsumableArray(targets).forEach(function (t) {
+    ref.bind(t, mapping);
+  });
+
+  return ref.proxy;
 };
 
 /***/ }),
--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import Ref from "./Ref";
 export const ref = function (target, vars, mapping) {
     return new Ref().bind(target, mapping).vars(vars).proxy;
 };
+
+export const refs = function (targets = [], vars, mapping) {
+    const ref = new Ref();
+    ref.vars(vars);
+    [...targets].forEach(t => {
+        ref.bind(t, mapping);
+    });
+    return ref.proxy;
+};
